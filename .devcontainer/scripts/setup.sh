e25ef8a46661998bf73df08990cb26a592832177
atom <atom@atom.com>
2023-11-05 16:15:50 +0000

    Development container no longer runs as root

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -9,20 +9,13 @@ Banner
 
 set -e
 
-root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo -e "Setup script must be run with root privileges. Please try again."
-        exit 1
-    fi
-}
-
 system() {
     apt-get update
     apt-get install -y zsh
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 
     # Change shell
-    chsh -s /bin/zsh root
+    chsh -s /bin/zsh atom
 }
 
 # @gdb
@@ -30,6 +23,5 @@ install_gdb() {
     apt-get install -y gdb
 }
 
-root
 system
 install_gdb
